f493789175ee75ddfa3c48433c584d62096bfe03
rodrihadjes14 <rodrihadjes@gmail.com>
2025-07-30 15:04:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const radioButtons = document.querySelectorAll('input[name="operation"]');
+  const propertyCards = document.querySelectorAll(".property-card");
+
+  radioButtons.forEach((radio) => {
+    radio.addEventListener("change", function () {
+      const selected = this.value;
+
+      propertyCards.forEach((card) => {
+        const type = card.getAttribute("data-operation");
+        card.style.display = type === selected ? "block" : "none";
+      });
+    });
+  });
+});
 
