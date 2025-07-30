d9ecc61f85f13d01fbb82c1c0beb35bac8ac1b76
rodrihadjes14 <rodrihadjes@gmail.com>
2025-07-30 16:29:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 document.addEventListener("DOMContentLoaded", function () {
   const radioButtons = document.querySelectorAll('input[name="operation"]');
   const propertyTypeSelect = document.querySelector('.property-type');
+  const searchInput = document.querySelector('.search-input');
   const propertyCards = document.querySelectorAll(".property-card");
 
   function filterCards() {
     const selectedOperation = document.querySelector('input[name="operation"]:checked').value;
-    const selectedType = propertyTypeSelect.value;
+    const selectedType = propertyTypeSelect.value.toLowerCase();
+    const searchText = searchInput.value.toLowerCase();
 
     propertyCards.forEach((card) => {
       const type = card.getAttribute("data-type");
       const op = card.getAttribute("data-operation");
+      const keywords = card.getAttribute("data-search");
 
       const matchesType = selectedType === "" || type === selectedType;
       const matchesOp = op === selectedOperation;
+      const matchesSearch = keywords.includes(searchText);
 
-      card.style.display = matchesType && matchesOp ? "grid" : "none";
+      card.style.display = matchesType && matchesOp && matchesSearch ? "grid" : "none";
     });
   }
 
@@ -23,4 +27,5 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   propertyTypeSelect.addEventListener("change", filterCards);
+  searchInput.addEventListener("input", filterCards);
 });
